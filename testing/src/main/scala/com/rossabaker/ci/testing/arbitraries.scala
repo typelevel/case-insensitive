4519939ec829223cb920a11da7aa4d9247da3e2f
Ross A. Baker <ross@rossabaker.com>
2020-05-03 22:32:12 -0400

    A more interesting Arbitrary[CIString]

--- testing/src/main/scala/com/rossabaker/ci/testing/arbitraries.scala
+++ testing/src/main/scala/com/rossabaker/ci/testing/arbitraries.scala
@@ -17,13 +17,25 @@
 package com.rossabaker.ci
 package testing
 
-import org.scalacheck.Arbitrary
+import java.util.Locale
+import org.scalacheck.{Arbitrary, Cogen, Gen}
 import org.scalacheck.Arbitrary.arbitrary
-import org.scalacheck.Cogen
 
 object arbitraries {
-  implicit val arbitraryForComRossabakerCIString: Arbitrary[CIString] =
-    Arbitrary(arbitrary[String].map(CIString(_)))
+  implicit val arbitraryForComRossabakerCIString: Arbitrary[CIString] = {
+    val chars = (Character.MIN_VALUE until Character.MAX_VALUE).map(_.toChar)
+    // These characters have odd properties when folding
+    val weirdCharFolds =
+      chars.filter(c => c.toUpper.toLower != c.toLower || c.toLower.toUpper != c.toUpper)
+    val weirdStringFolds = chars.filter(c =>
+      c.toString.toLowerCase(Locale.ROOT) != c.toLower.toString || c.toString.toUpperCase(
+        Locale.ROOT) != c.toUpper.toString)
+    // Also focus on characters that are cased at all
+    val lowers = chars.filter(_.isLower)
+    val uppers = chars.filter(_.isUpper)
+    val genChar = Gen.oneOf(weirdCharFolds, weirdStringFolds, lowers, uppers, arbitrary[Char])
+    Arbitrary(Gen.listOf(genChar).map(cs => CIString(cs.mkString)))
+  }
 
   implicit val cogenForComRossabakerCIString: Cogen[CIString] =
     Cogen[String].contramap(ci => new String(ci.toString.toArray.map(_.toLower)))
