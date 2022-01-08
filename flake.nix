136665389f84c7e6f0594036985f014cc236567e
Ross A. Baker <ross@rossabaker.com>
2022-01-08 11:22:31 -0500

    Flakier

--- flake.nix
+++ flake.nix
@@ -10,15 +10,21 @@
   outputs = { self, nixpkgs, flake-utils, typelevel-nix }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ typelevel-nix.overlay ];
+        };
         jekyll = pkgs.writeScriptBin "jekyll" ''
           ${pkgs.bundler}/bin/bundle && ${pkgs.bundler}/bin/bundle exec jekyll "$@"
         '';
       in
       {
-        devShell = typelevel-nix.devShells.${system}.library.overrideAttrs (old: {
-          buildInputs = old.buildInputs ++ [ jekyll ];
-        });
+        devShell = pkgs.devshell.mkShell {
+          imports = [ typelevel-nix.typelevel-shell ];
+          name = "my-project-shell";
+          typelevel-shell.jdk.package = pkgs.jdk8;
+          packages = [ jekyll ];
+        };
       }
     );
 }
