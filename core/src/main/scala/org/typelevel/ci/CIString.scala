c4a0a8f2b3844eeb4d0cced58026468b0bafc4a0
Henri Cook <github@henricook.com>
2022-10-08 13:57:15 +0100

    Update CIString.contains to pass the turkey test, drawing on commons-lang3's containsIgnoreCase

--- core/src/main/scala/org/typelevel/ci/CIString.scala
+++ core/src/main/scala/org/typelevel/ci/CIString.scala
@@ -98,7 +98,16 @@ final class CIString private (override val toString: String)
 
   def length: Int = toString.length
 
-  def contains(other: CIString): Boolean = toString.toLowerCase.contains(other.toString.toLowerCase)
+  def contains(needle: CIString): Boolean = {
+    val needleLength = needle.toString.length
+    val haystack = toString
+    // There's no point searching haystack indexes beyond which the substring is too small to ever match the needle
+    val maxHaystackStartingIndex = haystack.length - needleLength
+
+    (0 to maxHaystackStartingIndex).exists { i =>
+      haystack.regionMatches(true, i, needle.toString, 0, needleLength)
+    }
+  }
 
   @deprecated("Use toString", "0.1.0")
   def value: String = toString
